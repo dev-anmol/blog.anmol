316de3be23c6c9c9e51fc96ba76630dff70ee035
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-05-04 17:22:41 +0530

    udpated

--- src/components/create-blog/create-blog.component.ts
+++ src/components/create-blog/create-blog.component.ts
@@ -1,9 +1,12 @@
 import {Component, inject, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {AsyncPipe, CommonModule, JsonPipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-blog',
-  imports: [],
+  imports: [NgSelectModule, FormsModule],
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.css'
 })
@@ -16,7 +19,40 @@ export class CreateBlogComponent {
   category = signal('');
   author = signal('');
 
+  availableTags = [
+    { name: 'Tech' },
+    { name: 'Angular' },
+    { name: 'Life' },
+    { name: 'Health' }
+  ]
+  selectedTags: string[] = [];
 
+  addTagFn(name: string) {
+    return { name: name };
+  }
 
+  setTitle(){
+
+  }
+
+  setDescription() {
+
+  }
+
+  setContent() {
+
+  }
+
+  setImagePath() {
+
+  }
+
+  setCategory() {
+
+  }
+
+  setAuthor() {
+
+  }
 
 }
