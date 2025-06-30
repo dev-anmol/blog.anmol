67244fcb820b47d152531178bdca55bd8da349ea
Anmol <anmol.thakur@rightwatts.com>
2025-06-30 18:08:10 +0530

    gsap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import {Component, signal, WritableSignal} from '@angular/core';
-import {HeaderComponent} from '../components/header/header.component';
-import {SidebarComponent} from '../components/sidebar/sidebar.component';
-import {RouterOutlet} from '@angular/router';
-import {FooterComponent} from '../components/footer/footer.component';
-import {NgClass} from '@angular/common';
-import {ThemeService} from '../services/themeToggle/theme.service';
-import {Subscription} from 'rxjs';
-import {theme} from '../models/theme';
+import { NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { theme } from '../models/theme';
+import { ThemeService } from '../services/themeToggle/theme.service';
+import { gsap } from 'gsap';
+import LocomotiveScroll from 'locomotive-scroll';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +16,34 @@ import {theme} from '../models/theme';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'frontend';
   themeType: WritableSignal<theme> = signal('dark');
   private subscription!:Subscription;
+  private scroll!: LocomotiveScroll;
 
   constructor(private theme: ThemeService) {
     this.subscription = this.theme.themeListener$.subscribe((value: theme) => {
       this.themeType.set(value);
     })
   }
+
+  ngOnInit() {
+    const initLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      this.scroll = new LocomotiveScroll();
+    }
+
+    initLocomotiveScroll();
+  }
+
+  ngOnDestroy(): void {
+      if(this.scroll) {
+        this.scroll.destroy();
+      }
+  }
+
+
+
+
 }
--- src/components/intro/intro.component.ts
+++ src/components/intro/intro.component.ts
@@ -1,47 +1,82 @@
-import {Component, OnDestroy, signal, WritableSignal} from '@angular/core';
-import {techFormat} from '../../models/techFormat';
-import {content} from '../../models/content';
-import {CommonModule} from '@angular/common';
-import {HomeComponent} from '../home/home.component';
-import {ResumeComponent} from '../resume/resume.component';
-import {ProjectsComponent} from '../projects/projects.component';
-import {SkillsComponent} from '../skills/skills.component';
-import {ContactComponent} from '../contact/contact.component';
-import {Subscription} from 'rxjs';
-import {NavigateService} from '../../services/navigation/navigate.service';
-import {theme} from '../../models/theme';
-import {ThemeService} from '../../services/themeToggle/theme.service';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+  WritableSignal,
+} from '@angular/core';
+import { techFormat } from '../../models/techFormat';
+import { content } from '../../models/content';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { ResumeComponent } from '../resume/resume.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ContactComponent } from '../contact/contact.component';
+import { Subscription } from 'rxjs';
+import { NavigateService } from '../../services/navigation/navigate.service';
+import { theme } from '../../models/theme';
+import { ThemeService } from '../../services/themeToggle/theme.service';
+import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger, SplitText } from 'gsap/all';
 
 @Component({
   selector: 'app-intro',
-  imports: [CommonModule, HomeComponent, ResumeComponent, ProjectsComponent, SkillsComponent, ContactComponent],
+  imports: [
+    CommonModule,
+    HomeComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+  ],
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.css'
+  styleUrl: './intro.component.css',
 })
-export class IntroComponent implements OnDestroy{
-
-  contentType: WritableSignal<content> = signal("Home");
+export class IntroComponent implements OnDestroy, AfterViewInit, OnInit {
+  contentType: WritableSignal<content> = signal('Home');
   email: string = 'anmolll.thakurrr@gmail.com';
   location: string = 'Dwarka, New Delhi';
   private subscription!: Subscription;
   themeType: WritableSignal<theme> = signal('dark');
+  private ctx!: any;
+  @ViewChild('introText') introRef!: ElementRef;
 
-  constructor(private navigateToSection: NavigateService, private theme: ThemeService) {
-    this.subscription = this.navigateToSection.selectedCategory$.subscribe((value: content) => {
-      this.contentType.set(value);
-    })
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+      // gsap.registerPlugin(SplitText);
+      const timeline1 = gsap.timeline();
+      timeline1.from([this.introRef.nativeElement, '.logo', '.second'], { opacity: 0, duration: 1, filter: 'blur(10px)', stagger: 0.03 });
+    });
+  }
+
+  constructor(
+    private navigateToSection: NavigateService,
+    private theme: ThemeService
+  ) {
+    this.subscription = this.navigateToSection.selectedCategory$.subscribe(
+      (value: content) => {
+        this.contentType.set(value);
+      }
+    );
 
     this.subscription = this.theme.themeListener$.subscribe((value: theme) => {
       this.themeType.set(value);
-    })
+    });
   }
 
   updateContent(current: content) {
-    this.contentType.update(prev => prev = current);
+    this.contentType.update((prev) => (prev = current));
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+    this.ctx.revert();
   }
-
 }
